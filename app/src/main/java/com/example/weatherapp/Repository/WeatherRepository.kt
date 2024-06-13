14913aad9c6da87cc83828633d17ac920a7c6df4
Aban3049 <157634467+Aban3049@users.noreply.github.com>
2024-06-13 23:04:03 +0500

    initial commit

--- app/src/main/java/com/example/weatherapp/Repository/WeatherRepository.kt
+++ app/src/main/java/com/example/weatherapp/Repository/WeatherRepository.kt
@@ -5,6 +5,6 @@ import com.example.weatherapp.Server.ApiServices
 class WeatherRepository(val api: ApiServices) {
 
     fun getCurrentWeather(lat: Double, lng: Double, unit: String) =
-        api.getCurrentWeather(lat, lng, unit, "049dd217ac92a5a6705f6429d0918d4d")
+        api.getCurrentWeather(lat, lng, unit, "")
 
 }
